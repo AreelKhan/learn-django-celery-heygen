6a0881fe43bf8456dbcdd94697e8956e9d738103
Areel Khan <areel15511@gmail.com>
2024-10-10 19:06:39 -0400

    added register and home page

--- app/app/urls.py
+++ app/app/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from accounts import views as accounts_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', accounts_views.register, name='register'),
+    path('', accounts_views.home, name='home'),
 ]
